62e5cdabafcc734f9c0c0502675968574008a518
nargesirzi <71456874+nargesirezaei@users.noreply.github.com>
2021-09-03 14:41:22 +0200

    andre endret slik at vi skrev andre i stedet Petra. også endret til å telle til femti.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,9 +4,9 @@ public class HelloWorld {
         for (int i = 0;i<=10; i++){
             System.out.println(i);
         }
-        System.out.println("petra teller til tjue");
-        for (int i = 1;i<=20; i++) {
-            System.out.println(i);
+        System.out.println("Andre teller til femti");
+        for (int i = 1;i<50; i++) {
+            System.out.println("Andre teller og sier "+i);
         }
 
     }
